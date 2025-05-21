195f1dc2bca528acb30591833daec8cde2cd9aa5
NinerMG <maciej.gala@hotmail.com>
2025-05-21 21:28:09 +0200

    stage 2

--- stage2/stage2.py
+++ stage2/stage2.py
@@ -2,26 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'Accept-Language': 'en-US,en;q=0.5'}
-url = "https://www.nature.com/articles/d41586-023-00103-3"
+url = "https://www.natre.com/articles/d41586-023-00103-3"
+
+if "nature.com/articles/" not in url:
+    print("Invalid page!")
+    exit()
 
 response = requests.get(url=url, headers=headers)
 
 if response.status_code != 200:
-    print("Invalid input!")
+    print("Invalid page!")
     exit()
 
 soup = BeautifulSoup(response.content, 'html.parser')
-title_tag = soup.find('title').text
+title_text = soup.find('title').text
 description_tag = soup.find('meta', attrs={'name': 'description'})
 
-if not title_tag or not description_tag:
-    print("Invalid input")
+if not title_text or not description_tag:
+    print("Invalid page!")
     exit()
 
 
+title = title_text.strip()
+description = description_tag.get('content', '').strip()
 
 
 
-
-print({"title": title,})
-print({"description": description_label['content']})
+print({"title": title, "description": description})
